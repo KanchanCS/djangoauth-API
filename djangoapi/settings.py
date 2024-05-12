e575b17251b3db16a66a5bf0fc7b6f7c35f09041
KanchanCS <kanchan111yadav@gmail.com>
2024-05-12 10:09:26 +0530

    user profile api

--- djangoapi/settings.py
+++ djangoapi/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from pathlib import Path
 from datetime import timedelta
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -163,4 +163,5 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://127.0.0.1:9000",
+   
 ]
